c1de9ca9ab6ac155dcd37a346998c1a1f5206533
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-01-30 13:59:04 +0800

    支持视频播放！

--- index.core.js
+++ index.core.js
@@ -1,8 +1,30 @@
 ﻿const sleep = delay => new Promise((resolve) => setTimeout(resolve, delay))
+const rawOpen = window.open;
+window.open = (url, target, feature) => {
+    if (url.indexOf("player.html") > 1) {
+        var videoFrame = document.createElement("iframe");
+        videoFrame.frameBorder = 0;
+        videoFrame.style = "z-index:9999;position:fixed;backdrop-filter: blur(10px) brightness(100%);background-color: rgba(255, 255, 255, .6);width:100%;margin-top:0px;height:100%;left:0px;right:0px;top:0px;";
+        document.body.appendChild(videoFrame);
+        videoFrame.src = "https://static.xuexi.cn/search/online/index.html";
+        var clsBtn = document.createElement("img");
+        clsBtn.style = "z-index:10000;filter: invert(100%);position:fixed;display: inline-block;right:0px;top:0px;float:right;height:32px;width:32px;transition:background-color 0.2s;"
+        clsBtn.className = "barBtn"
+        clsBtn.src = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDI0IDI0IiBoZWlnaHQ9IjQ4cHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjQ4cHgiIGZpbGw9IiMwMDAwMDAiPjxyZWN0IGZpbGw9Im5vbmUiIGhlaWdodD0iMjQiIHdpZHRoPSIyNCIvPjxwYXRoIGQ9Ik0yMiwzLjQxbC01LjI5LDUuMjlMMjAsMTJoLThWNGwzLjI5LDMuMjlMMjAuNTksMkwyMiwzLjQxeiBNMy40MSwyMmw1LjI5LTUuMjlMMTIsMjB2LThINGwzLjI5LDMuMjlMMiwyMC41OUwzLjQxLDIyeiIvPjwvc3ZnPg==";
+        document.body.appendChild(clsBtn);
+        clsBtn.onclick = function () {
+            document.body.removeChild(videoFrame);
+            document.body.removeChild(clsBtn);
+        }
+        videoFrame.src = url;
+    } else {
+        rawOpen(url, target, feature);
+    }
+}
 const init = async () => {
     if (navigator.serviceWorker && !navigator.serviceWorker.controller) {
         await navigator.serviceWorker.register('./sw.js', { scope: '/' });
-        document.getElementById("status").innerText="正在安装Service Worker..."
+        document.getElementById("status").innerText = "正在安装Service Worker..."
         await sleep(3000)
         document.location.reload();
     }
@@ -12,6 +34,6 @@ const init = async () => {
     let renpyScript = document.createElement("script");
     document.body.appendChild(renpyScript);
     renpyScript.src = "./index.js";
-    document.getElementById("status").innerText="正在加载Ren'Py..."
+    document.getElementById("status").innerText = "正在加载Ren'Py..."
 };
 init();
